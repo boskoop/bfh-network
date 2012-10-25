f245e01c0da21da01a36d733ec49698c2cda9aa6
Christian Bürgi <burgc5@bfh.ch>
2012-10-26 00:49:24 +0200

    added a shutdown hook which listens on UDP

--- TcpServer/main.c
+++ TcpServer/main.c
@@ -10,28 +10,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PORT "20911"
+#define SERVER_PORT "20911"
+#define SHUTDOWN_PORT "20912"
 #define BUFFER_SIZE 128
 
-void *handlerThread(void *tcpSocket) {
+static int runFlag = 1;
+
+void *shutdownHookThread(void *p) {
+    struct sockaddr address;
+    int addressLength = sizeof (address);
+    char shutdown[] = "shutdown\n";
+    char okMessage[] = "initializing shutdown\n";
+    char nokMessage[] = "invalid command, use 'shutdown' to terminate\n";
+    SOCKET socket = passiveUDP(SHUTDOWN_PORT);
+    int returnCode;
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof (buffer));
+
+    while (1) {
+        returnCode = recvfrom(socket, buffer, sizeof (buffer), 0, &address, &addressLength);
+        if (strncmp(shutdown, buffer, strlen(shutdown)) == 0) {
+            sendto(socket, &okMessage, strlen(okMessage), 0, (struct sockaddr *) &address, (socklen_t) addressLength);
+            break;
+        }
+        sendto(socket, &nokMessage, strlen(nokMessage), 0, (struct sockaddr *) &address, (socklen_t) addressLength);
+    }
+}
+
+void *workerThread(void *tcpSocket) {
     char hello[] = "Thank you for registering\n";
+    char shutdown[] = "Server shutting down..\n";
     int returnCode;
     char buffer[BUFFER_SIZE];
     
     SOCKET socket = *(SOCKET *) tcpSocket;
-    
+
+    memset(buffer, 0, sizeof (buffer));
+
     send(socket, hello, strlen(hello), 0);
-    while (1) {
+    while (runFlag) {
         returnCode = recv(socket, buffer, sizeof (buffer), 0);
         if (returnCode <= 0 || buffer[0] == '\n') {
             break;
         }
         send(socket, hello, strlen(hello), 0);
     }
+    if (runFlag == 0) {
+        send(socket, shutdown, strlen(shutdown), 0);
+    }
+
+    close(socket);
     
     return NULL;
 }
-
 
 /*
  * 
@@ -40,26 +71,32 @@ int main(int argc, char** argv) {
 
     struct sockaddr address;
     int addressLength = sizeof (address);
-    pthread_t workerThread;
+    pthread_t worker, shutdownHook;
 
     SOCKET s, ss;
     WSAinit();
-    ss = passiveTCP(PORT, 5);
+    ss = passiveTCP(SERVER_PORT, 5);
 
     s = accept(ss, &address, &addressLength);
+
+    pthread_create(&worker, NULL, workerThread, (void *) &s);
+
+    pthread_create(&shutdownHook, NULL, shutdownHookThread, (void *) NULL);
+
+    pthread_join(shutdownHook, NULL);
     
-    pthread_create(&workerThread, NULL, handlerThread, (void *) &s);
-    
-    pthread_join(workerThread, NULL);
-    
-//    send(s, hello, strlen(hello), 0);
-//    while (1) {
-//        returnCode = recv(s, buffer, sizeof (buffer), 0);
-//        if (returnCode <= 0 || buffer[0] == '\n') {
-//            break;
-//        }
-//        send(s, buffer, returnCode, 0);
-//    }
+    runFlag = 0;
+
+    pthread_join(worker, NULL);
+
+    //    send(s, hello, strlen(hello), 0);
+    //    while (1) {
+    //        returnCode = recv(s, buffer, sizeof (buffer), 0);
+    //        if (returnCode <= 0 || buffer[0] == '\n') {
+    //            break;
+    //        }
+    //        send(s, buffer, returnCode, 0);
+    //    }
     closesocket(ss);
 
     return (EXIT_SUCCESS);
